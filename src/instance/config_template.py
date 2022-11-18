dbd94ac37e18adc629f006558c7a5220303e213f
MikeTsenatek <10235033+MikeTsenatek@users.noreply.github.com>
2022-11-18 16:03:01 +0100

    add default config

---
+++ src/instance/config_template.py
@@ -0,0 +1,21 @@
+prefix = 'https://api.telegram.org/bot'
+key = ''
+
+GETURL = prefix + key + '/getUpdates'
+SENDURL = prefix + key + '/sendMessage'
+EDITURL = prefix + key + '/editMessageText'
+SETWEBHOOKURL = prefix + key + '/setWebhook'
+AVATARURL = prefix + key + '/getUserProfilePhotos'
+GETFILE = prefix + key + '/getFile'
+FILEPREFIX = 'https://api.telegram.org/file/bot' + key + '/'
+WEBHOOKURL = 'https://poll.nerdberg.de/api/telegram'
+
+TEXT = 'Hast du heute \($day\) vor in den Nerdberg zu kommen?'
+OPTIONS = (
+    "Ja, den ganzen Abend",
+    "Ja ab 21 Uhr",
+    "Ja aber nur vor 21 Uhr",
+    "Ja aber nur wenn es nicht so voll ist",
+    "Nein"
+)
+SQLALCHEMY_DATABASE_URI = 'sqlite:///sqlite.db'
