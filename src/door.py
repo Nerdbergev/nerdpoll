cffb96301ce889dbbab4dabc6a2a4fe97c85e53a
MikeTsenatek <10235033+MikeTsenatek@users.noreply.github.com>
2022-11-18 23:40:51 +0100

    Fix doort state

--- src/door.py
+++ src/door.py
@@ -6,8 +6,11 @@ last_door_check = 0
 last_door_status = None
 
 def is_door_open():
-	now = time.time()
-	if last_door_check + 60 < now:
-		last_door_status = requests.get(app.config['DOORURL']).json()["results"][0]["status"] == 'open'
-		last_door_check = now
-	return last_door_status
+    global last_door_check, last_door_status
+    now = time.time()
+    if last_door_check + 60 < now:
+        request = requests.get(app.config['DOORURL'])
+        last_door_status = request.json()["state"]["open"] == True
+        last_door_check = now
+    print(last_door_status)
+    return last_door_status
